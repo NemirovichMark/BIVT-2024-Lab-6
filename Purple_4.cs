43b44cfc2e28e7a382a54db80bad2efb179849af
ByakuByaku <129057299+ByakuByaku@users.noreply.github.com>
2025-03-05 23:37:27 +0300

    Update Purple_4.cs

--- Purple_4.cs
+++ Purple_4.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -47,8 +47,13 @@ namespace Lab_6
             private Sportsman[] _sportsmen;
 
             public string Name => _name;
-            public Sportsman[] Sportsmen => _sportsmen;
-            
+            public Sportsman[] Sportsmen
+            {
+                get
+                {
+                    return _sportsmen;
+                }
+            }
 
             public Group(string name)
             {
@@ -57,17 +62,94 @@ namespace Lab_6
             }
             public Group(Group group)
             {
-                _name = group.Name;
+                _name=group.Name;
+                var copy = group.Sportsmen;
+                _sportsmen = new Sportsman[0];
                 if (group.Sportsmen == null) _sportsmen = null;
-                else Array.Copy(_sportsmen,group._sportsmen, _sportsmen.Length);
+                Array.Copy(copy, _sportsmen, _sportsmen.Length);
+
             }
-            
+
+            public void Add(Sportsman sportsman)
+            {
+                if (_sportsmen == null) return;
+                Array.Resize(ref _sportsmen,_sportsmen.Length+1);
+                _sportsmen[_sportsmen.Length-1] = sportsman;
+
+            }
+            public void Add(Sportsman[] sportsman)
+            {
+                if (_sportsmen == null || sportsman.Length==0) return;
+                int index = _sportsmen.Length;
+                Array.Resize(ref _sportsmen,_sportsmen.Length+sportsman.Length);
+                int a = 0;
+                for (int i = index;i < _sportsmen.Length; i++)
+                {
+                    _sportsmen[i] = sportsman[a++];
+                }
+
+            }
+            public void Add(Group group)
+            {
+                if (_sportsmen == null || group.Sportsmen == null)
+                    return;
+                Add(group.Sportsmen);
+            }
+
             public void Sort()
-            {   
-                Array.Copy(_sportsmen.OrderBy(x => x.Time).ToArray(), _sportsmen, _sportsmen.OrderBy(x => x.Time).ToArray().Length);
+            {
+                if (_sportsmen != null)
+                    _sportsmen = _sportsmen.OrderBy(r => r.Time).ToArray();
+            }
+            public static Group Merge(Group group1, Group group2)
+            {
+                var group = new Group();
+                if (group1.Sportsmen == null || group2.Sportsmen == null)
+                {
+                    if (group1.Sportsmen != null)
+                    {
+                        Array.Copy(group1._sportsmen, group.Sportsmen, group1._sportsmen.Length);
+                    }
+                    else if (group2.Sportsmen != null)
+                    {
+                        Array.Copy(group2._sportsmen, group.Sportsmen, group2._sportsmen.Length);
+                    }
+
+                    return group;
+                }
+                Array.Resize(ref group._sportsmen, group1._sportsmen.Length+ group2._sportsmen.Length);
+                int i = 0, j = 0, k = 0;
+                while (i < group1._sportsmen.Length && j < group2._sportsmen.Length)
+                {
+                    if (group1._sportsmen[i].Time <= group2._sportsmen[j].Time)
+                        group._sportsmen[k++] = group1._sportsmen[i++];
+                    else
+                        group._sportsmen[k++] = group2._sportsmen[j++];
+                }
+                while (i < group._sportsmen.Length)
+                    group._sportsmen[k++] = group1._sportsmen[i++];
+                while (j < group2._sportsmen.Length)
+                    group._sportsmen[k++] = group2._sportsmen[j++];
+                return group;
+
+
+
             }
 
+            public void Print()
+            {
+                Console.WriteLine($"Name: {_name}");
 
+                Console.WriteLine();
+                Console.WriteLine();
+
+                Console.WriteLine("Sportsmen");
+
+                foreach(var sportsman in _sportsmen)
+                {
+                    sportsman.Print();
+                }
+            }
         }
     }
 }
