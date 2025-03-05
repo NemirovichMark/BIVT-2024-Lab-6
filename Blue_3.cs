a957421dbc9e8e51e5bb043ab707b27480a41f67
ingvar0 <igor0876321@gmail.com>
2025-03-05 13:13:37 +0300

    Add files via upload

---
+++ Blue_3.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6
+{
+    public class Blue_3
+    {
+        public struct Participant
+        {
+            // поля
+            private readonly string _name;
+            private readonly string _surname;
+            public readonly int[] _penaltyTime;
+
+            // конструктор
+            public Participant(string name, string surname)
+            {
+                _name = name;
+                _surname = surname;
+                _penaltyTime = new int[10];
+                for(int i = 0; i < 10; i++)
+                {
+                    _penaltyTime[i] = 0;
+                }
+            }
+
+            // свойства
+            private string Name => _name;
+            private string Surname => _surname;
+            public int[] PenaltyTimes
+            {
+                get
+                {
+                    if (_penaltyTime == null) return null;
+                    int[] newArray = new int[_penaltyTime.Length];
+                    Array.Copy(_penaltyTime, newArray, _penaltyTime.Length);
+                    return newArray;
+                }
+            }
+
+            public int TotalTime
+            {
+                get
+                {
+                    if (_penaltyTime == null) return 0;
+                    int s = 0;
+                    for (int i = 0; i < _penaltyTime.Length; i++)
+                    {
+                        s += _penaltyTime[i];
+                    }
+                    return s;
+                }
+            }
+
+            public bool IsExpelled
+            {
+                get
+                {
+                    if (_penaltyTime == null) return false;
+                    for (int i = 0; i < _penaltyTime.Length; i++)
+                    {
+                        if (_penaltyTime[i] == 10) return true;
+                    }
+                    return false;
+                }
+            }
+
+            // методы
+            public void PlayMatch(int time)
+            {
+                if (time == null || time < 0) return;
+
+                for (int i = 0; i < 10; i++)
+                {
+                    if (_penaltyTime[i] == 0)
+                    {
+                        _penaltyTime[i] = time;
+                    }
+                }
+            }
+
+            public static void Sort(Participant[] array)
+            {
+                if (array == null) return;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    for (int j = i; j < array.Length; j++)
+                    {
+                        if (array[i].TotalTime > array[j].TotalTime)
+                        {
+                            Participant temp = array[i];
+                            array[i] = array[j];
+                            array[j] = temp;
+                        }
+                    }
+                }
+            }
+
+            public void Print()
+            {
+                Console.WriteLine($"{_name} {_surname} {TotalTime} {IsExpelled}");
+            }
+        }
+    }
+}
---
+++ Blue_5.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6
+{
+    public class Blue_5
+    {
+        public struct Sportsman
+        {
+            private string _name;
+            private string _surname;
+            private int _place;
+
+            public string Name { get => _name; }
+            public string Surname { get => _surname; }
+            public int Place { get => _place; }
+
+            public Sportsman(string name, string surname)
+            {
+                _name = name;
+                _surname = surname;
+                _place = 0;
+            }
+
+            public void SetPlace(int place)
+            {
+                _place = place;
+            }
+
+
+            public void Print()
+            {
+                Console.WriteLine($"{Name} {Surname} {Place}");
+            }
+        }
+
+        public struct Team
+        {
+            private string _name;
+            private Sportsman[] _sportsmen;
+            private int _k;
+
+            public string Name { get => _name; }
+            public Sportsman[] Sportsmen { get => _sportsmen; }
+
+            public int SummaryScore
+            {
+                get
+                {
+                    if (_sportsmen == null || _k == 0) return 0;
+                    int score = 0;
+                    foreach (var s in _sportsmen)
+                    {
+                        switch (s.Place)
+                        {
+                            case 1: score += 5; break;
+                            case 2: score += 4; break;
+                            case 3: score += 3; break;
+                            case 4: score += 2; break;
+                            case 5: score += 1; break;
+                            default: break;
+                        }
+                    }
+                    return score;
+                }
+            }
+
+            public int TopPlace
+            {
+                get
+                {
+                    if (_sportsmen == null) return 0;
+
+                    int topPlace = 18;
+                    foreach (var s in _sportsmen)
+                    {
+                        if (s.Place < topPlace) topPlace = s.Place;
+                    }
+                    return topPlace;
+                }
+            }
+
+            public Team(string name)
+            {
+                _name = name;
+                _sportsmen = new Sportsman[6];
+                _k = 0;
+            }
+
+            public void Add(Sportsman sportsman)
+            {
+                if (_sportsmen == null || _k >= 6) return;
+
+                if (_k < 6)
+                {
+                    _sportsmen[_k] = sportsman;
+                    _k++;
+                }
+            }
+
+            public void Add(Sportsman[] sportsmen)
+            {
+                if (sportsmen == null || _k >= 6) return;
+
+                for (int i = 0; i < _sportsmen.Length; i++)
+                {
+                    if (_k >= 6) return;
+                    if (_k < 6)
+                    {
+                        _sportsmen[_k++] = sportsmen[i];
+                    }
+                }
+            }
+
+            public void Print()
+            {
+                Console.WriteLine($"Команда: {Name}");
+                Console.WriteLine("Состав команды:");
+                foreach (var s in _sportsmen)
+                {
+                    s.Print();
+                }
+                Console.WriteLine($"Общее количество баллов: {SummaryScore}");
+                Console.WriteLine($"Лучшее место: {TopPlace}\n");
+            }
+
+            public static void Sort(Team[] teams)
+            {
+                for (int i = 0; i < teams.Length; i++)
+                {
+                    for (int j = 0; j < teams.Length - i - 1; j++)
+                    {
+                        if (teams[j].SummaryScore < teams[j + 1].SummaryScore ||
+                            teams[j].SummaryScore == teams[j + 1].SummaryScore && teams[j].TopPlace > teams[j + 1].TopPlace)
+                        {
+                            Team x = teams[j];
+                            teams[j] = teams[j + 1];
+                            teams[j + 1] = x;
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
