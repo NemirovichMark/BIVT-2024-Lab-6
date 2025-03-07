3fbe7454d7bd904efad9d2bdb51d2da012978180
AndrejWP <m2403371@edu.misis.ru>
2025-03-07 22:55:09 +0300

    Add files via upload

---
+++ White_2.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.Expando;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6
+{
+    public class White_2
+    {
+        public struct Participant
+        {
+            // поля 
+            private string _surname;
+            private string _name;
+            private double _firstJump;
+            private double _secondJump;
+            //свойства
+            public string Surname
+            {
+                get
+                {
+                    if (_surname == null) return default;
+                    return _surname;
+                }
+            }
+            public string Name
+            {
+                get
+                {
+                    if (_name == null) return default;
+                    return _name;
+                }
+            }
+            
+            public double FirstJump => _firstJump;
+            public double SecondJump => _secondJump;
+            public double BestJump => Math.Max(_firstJump, _secondJump);
+            public Participant(string name, string surname)
+            {
+                _name = name;
+                _surname = surname;
+                _firstJump = 0;
+                _secondJump = 0;
+            }
+            public static void Sort(Participant[] array)
+            {
+                if (array == null || array.Length == 0) return;
+                int n = array.Length;
+                bool y;
+                for (int i = 0; i < n - 1; i++)
+                {
+                    y = false;
+                    for (int j = 0; j < n - 1 - i; j++)
+                    {
+                        if (array[j].BestJump < array[j + 1].BestJump) 
+                        {
+                            (array[j], array[j + 1]) = (array[j + 1], array[j]);
+                            y = true;
+                        }
+                    }
+                    if (!y) break; 
+                }
+            }
+            public void Jump(double result)
+            {
+                if (_firstJump == 0)
+                {
+                    _firstJump = result;
+                }
+                else if (_secondJump == 0)
+                {
+                    _secondJump = result;
+                }
+            }
+            public void Print()
+            {
+                Console.WriteLine($"Имя: {_name}, Фамилия: {_surname}, Первый прыжок: {_firstJump}, Второй прыжок: {_secondJump}, Лучший прыжок: {BestJump}");
+            }
+        }
+    }
+}
---
+++ White_3.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.Expando;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6
+{
+    public class White_3
+    {
+        public struct Student
+        {
+            private string _name;
+            private string _surname;
+            private int[] _marks;
+            private int _skipped;
+            public string Surname
+            {
+                get
+                {
+                    if (_surname == null) return default;
+                    return _surname;
+                }
+            }
+            public string Name
+            {
+                get
+                {
+                    if (_name == null) return default;
+                    return _name;
+                }
+            }
+            public double AvgMark
+            {
+                get
+                {
+                    if (_marks == null || _marks.Length == 0)
+                    {
+                        return 0;
+                    }
+                    return (double)_marks.Sum() / _marks.Length; 
+                }
+            }
+            public int Skipped => _skipped;
+            public Student(string name, string surname)
+            {
+                _name = name;
+                _surname = surname;
+                _marks = new int[0];
+                _skipped = 0;
+            }
+
+            public void Lesson(int mark)
+            {
+                if (mark == 0)
+                {
+                    _skipped++;
+                }
+                else
+                {
+                    if (_marks == null) return;
+                    int[] new_marks = new int[_marks.Length + 1];
+                    for (int i = 0; i < _marks.Length; i++)
+                    {
+                        new_marks[i] = _marks[i];
+                    }
+
+                    new_marks[new_marks.Length - 1] = mark;
+                    _marks = new_marks;
+                }
+            }
+            public static void SortBySkipped(Student[] array)
+            {
+                if (array == null || array.Length == 0) return;
+                int n = array.Length;
+                bool y;
+                for (int i = 0; i < n - 1; i++)
+                {
+                    y = false;
+                    for (int j = 0; j < n - 1 - i; j++)
+                    {
+                        if (array[j].Skipped < array[j + 1].Skipped)
+                        {
+                            (array[j], array[j + 1]) = (array[j + 1], array[j]);
+                            y = true;
+                        }
+                    }
+                    if (!y) break;
+                }
+
+            }
+            public void Print()
+            {
+                Console.WriteLine($"Имя: {_name}, Фамилия: {_surname}, Средняя оценка: {AvgMark:F2}, Пропуски: {_skipped}");
+            }
+
+        }
+    }
+}
