e0f390bc82c974d246d22fbc305ea2fe7641c71d
ioannya <ivank8037@gmail.com>
2025-03-07 16:27:04 +0300

    Add files via upload

---
+++ Green_1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6
+{
+    public class Green_1
+    {
+        public struct Participant
+        {
+            private string _surname;
+            private string _group;
+            private string _teacher;
+            private double _result;
+
+            private static readonly double standart;
+            private static int passedcount;
+
+            static Participant()
+            {
+                standart = 100; 
+                passedcount = 0;
+            }
+
+            public Participant(string surname, string group, string teacher)
+            {
+                _surname = surname;
+                _group = group;
+                _teacher = teacher;
+                _result = 0;
+            }
+
+            public string Surname
+            {
+                get 
+                { 
+                    //if(_surname == null) return default;
+                    return _surname; 
+                }
+            }
+
+            public string Group
+            {
+                get
+                {
+                    //if (_group == null) return default;
+                    return _group;
+                }
+            }
+
+            public string Teacher
+            {
+                get
+                {
+                    //if ( _teacher == null) return default;
+                    return _teacher;
+                }
+            }
+
+            public double Result
+            {
+                get
+                {
+                    //if(_result == null) return default;
+                    return _result;
+                }
+            }
+
+            public static int PassedTheStandard => passedcount;
+
+            public bool HasPassed => _result <= standart && _result > 0;
+
+            public void Run(double result)
+            {
+                if (_result == 0)
+                {
+                    _result = result;
+                    if (HasPassed)
+                    {
+                        passedcount++;
+                    }
+                }
+            }
+
+            public void Print()
+            {
+                Console.WriteLine($"Фамиия: {Surname}");
+                Console.WriteLine($"Группа: {Group}");
+                Console.WriteLine($"Учитель: {Teacher}");
+                Console.WriteLine($"Результат: {Result}");
+                Console.WriteLine($"Прошли: {HasPassed}");
+            }
+        }
+    }
+}
---
+++ Green_2.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6
+{
+    public class Green_2
+    {
+        public struct Student
+        {
+            private string _name;
+            private string _surname;
+            private int[] _marks;
+            private int _count;
+
+            public Student(string name, string surname)
+            {
+                _name = name;
+                _surname = surname;
+                _marks = new int[4];
+                _count = 0;
+            }
+
+            public string Name
+            {
+                get
+                {
+                    if (_name == null) return default;
+                    return _name;
+                }
+            }
+            public string Surname
+            {
+                get
+                {
+                    if ( _surname == null) return default;
+                    return _surname;
+                }
+            }
+            public int[] Marks
+            {
+                get
+                {
+                    if( _marks == null) return default;
+                    return _marks.ToArray();
+                }
+            }
+            public double AvgMark
+            {
+                get
+                {
+                    if(_marks.Length == 0 || _marks == null) return default;
+                    double s = 0;
+                    foreach (var m in _marks)
+                    {
+                        s += m;
+                    }
+                    return s / _marks.Length;
+                }
+            }
+
+            public bool IsExcellent
+            {
+                get
+                {
+                    if(_count < _marks.Length) return false;
+                    foreach (var m in _marks)
+                    {
+                        if (m < 4) return false;
+                    }
+                    return true;
+                }
+            }
+
+            public void Exam(int mark)
+            {
+                if (_count < _marks.Length && mark <= 5 && mark >= 2)
+                {
+                    _marks[_count] = mark;
+                    _count++;
+                }
+            }
+
+            public static void SortByAvgMark(Student[] array)
+            {
+                if(array == null || array.Length == 0) return;
+                int n = array.Length;
+                for (int i = 0; i < n - 1; i++)
+                {
+                    for (int j = 0; j < n - i - 1; j++)
+                    {
+                        if (array[j].AvgMark < array[j + 1].AvgMark)
+                        {
+                            Student temp = array[j];
+                            array[j] = array[j + 1];
+                            array[j + 1] = temp;
+                        }
+                    }
+                }
+            }
+            public void Print()
+            {
+                Console.WriteLine($"Имя: {Name}, Фамилия: {Surname}, Средний балл: {AvgMark}, Отличники: {IsExcellent}");
+            }
+        }
+    }
+}
