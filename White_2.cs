298b328494a8013d8798a4fc24effb33b92f7197
AndrejWP <m2403371@edu.misis.ru>
2025-03-12 21:25:42 +0300

    Update White_2.cs

--- White_2.cs
+++ White_2.cs
@@ -1,10 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices.Expando;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Xml.Linq;
 namespace Lab_6
 {
     public class White_2
